ac9e72f58363163e7fe6a3137e7997d702f3ac0d
harshvachhani1 <harshv@webelignt.co.in>
2023-10-20 19:15:57 +0530

    KoolReport demo

---
+++ app/Reports/MyReport.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Reports;
+
+use App\Models\Lead;
+use koolreport\laravel\Friendship;
+
+class MyReport extends \koolreport\KoolReport
+{
+    use Friendship;
+    // By adding above statement, you have claim the friendship between two frameworks
+    // As a result, this report will be able to accessed all databases of Laravel
+    // There are no need to define the settings() function anymore
+    // while you can do so if you have other datasources rather than those
+    // defined in Laravel.
+    function settings()
+    {
+        return array(
+            "dataSources"=>array(
+                "elo"=>array(
+                    "class"=>'\koolreport\laravel\Eloquent', // This is important
+                )
+            )
+        );
+    }
+
+    function setup()
+    {
+        // Let say, you have "sale_database" is defined in Laravel's database settings.
+        // Now you can use that database without any futher setitngs.
+        $this->src("elo")->query(
+            Lead::join('campaigns','campaigns.id','=','leads.user_id')
+            ->join('lead_quotes','lead_quotes.lead_id','=','leads.id')
+            ->join('quotes','quotes.id','=','lead_quotes.quote_id')
+            ->take(10)
+            )
+        ->pipe($this->dataStore("lead"));
+    }
+}
